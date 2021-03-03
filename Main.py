84823c8ac1372590b203c6ce165b0d99367ee2c4
ikrut555 <44076851+ikrut555@users.noreply.github.com>
2021-03-03 09:34:44 +0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -2,7 +2,14 @@
 # Developers:   Aksenov A. (%),
 #               Soloveychik D. (%),
 #               Labuzov A. (%)
-
+import urllib.request
+url = 'https://www.nfl.com/players/bryce-petty/stats/'
+f = urllib.request.urlopen(url)
+s = f.read()
+text = str(s)
+part_name = text.find("nfl-c-player-header__title")
+name = text[text.find('>',part_name)+1:text.find('</h1',part_name)]
+print(name)
 
 
 
