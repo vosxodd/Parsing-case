a9a165fa9d65e6c4b331f3fc2e3ac0992258f42a
ikrut555 <44076851+ikrut555@users.noreply.github.com>
2021-03-03 09:41:45 +0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -4,14 +4,15 @@
 #               Labuzov A. (%)
 import urllib.request
 with open("input.txt") as f:
-  for line in f:
-    url = line
-    f = urllib.request.urlopen(url)
-    s = f.read()
-    text = str(s)
-    part_name = text.find("nfl-c-player-header__title")
-    name = text[text.find('>',part_name)+1:text.find('</h1',part_name)]
-    print(name)
+  with open("output.txt","w") as f_out:
+    for line in f:
+      url = line
+      f = urllib.request.urlopen(url)
+      s = f.read()
+      text = str(s)
+      part_name = text.find("nfl-c-player-header__title")
+      name = text[text.find('>',part_name)+1:text.find('</h1',part_name)]
+      print(name,file=f_out)
 
 
 
